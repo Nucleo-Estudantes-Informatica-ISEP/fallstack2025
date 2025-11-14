45088952ac87de72e5ed560985b188c92fe98e3f
1241273-hugo2006alm <1241273@isep.pt>
2025-11-14 16:40:42 +0000

    fix: added old footer + added new linktree icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,8 +26,7 @@ const App: FunctionComponent = () => {
         <Content contentRef={contentRef} />
         <HeadsUp />
         <Footer
-          lastEditionUrl="https://fallstack2023.nei-isep.org/"
-          neiSimplifiedLogo={NeiLogoSimplifiedWhite}
+          neiLogoSrc={NeiLogoSimplifiedWhite}
         />
       </GenericContainer>
     </HeroContainer>
